dc2c7ef30ae62375c0aa91089cbb6c3bb0fa2efc
Bibek <bibekg@ucla.edu>
2018-04-04 11:38:35 -0700

    Initial commit

---
+++ src/js/App.js
@@ -0,0 +1,13 @@
+// @flow
+
+import * as React from 'react'
+import Button from './components/Button'
+import LogInButton from './components/LogInButton'
+
+// Uncomment the two lines below as you build the components and are ready to test them.
+export default () => (
+    <div>
+        {/* <Button primary>Hello world!</Button>  */}
+        {/* <LogInButton /> */}
+    </div>
+)
---
+++ src/js/index.js
@@ -0,0 +1,11 @@
+// @flow
+
+import * as React from "react"
+import { render } from "react-dom"
+import App from './App'
+
+const wrapper = document.getElementById("app");
+
+if (wrapper) {
+    render(<App />, wrapper)
+}
